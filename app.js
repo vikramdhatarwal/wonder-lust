3dfa46e1fea31dc61943eb3a7ec6e220c9780638
Vikram Dhatarwal <vikk.choudhary06@gmail.com>
2026-06-19 23:49:18 +0530

    Initial Setup: connection to DB

--- app.js
+++ app.js
@@ -3,10 +3,27 @@ const app=express();
 const mongoose=require("mongoose");
 const path=require("path");
 const ejs=require("ejs");
+
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
 });
 
+
 app.get("/",(req,res)=>{
     res.send("Hi,I am root");
 });
+
+
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+
+
+main().then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.error("Error connecting to MongoDB:", err);
+});
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+
+}
